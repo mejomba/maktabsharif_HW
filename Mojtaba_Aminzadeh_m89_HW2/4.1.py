cbb797ce91f27b855b90184ddde19d6b42853b0f
mejomba <moojaminzadeh@gmail.com>
2022-11-15 09:53:18 +0330

    a

---
+++ Mojtaba_Aminzadeh_m89_HW2/4.1.py
@@ -0,0 +1,42 @@
+from string import digits
+from functools import reduce
+from time import sleep
+
+def unique_number(numbers):
+    try:
+        return reduce(lambda x, y: x + y, set(numbers))
+    except:
+        return ''
+
+
+def count_number(numbers):
+    s = ''
+    for d in digits:
+        s += str(numbers.count(d))
+    try:
+        return reduce(lambda x, y: x + y ,filter(lambda x: x > '1', s))
+    except:
+        return ''
+
+
+def sort_number(numbers):
+    try:
+        return reduce(lambda x, y: x + y, sorted(numbers))
+    except:
+        return ''
+
+
+data = '442254545'
+history = None
+while data != history:
+    print(data)
+    history = data
+    data = sort_number(count_number(data) + unique_number(data))
+    sleep(0.5)
+# output
+# 442254545
+# 223445
+# 222345
+# 23345
+# 23452
+# 22345
