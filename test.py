46d02c5730eefea03bf2cb9bbd024c8ec4cddfe0
mejomba <moojaminzadeh@gmail.com>
2022-12-12 22:31:16 +0330

    a

--- test.py
+++ test.py
@@ -1,3 +1,20 @@
 import threading
 
-threading.Lock()
+
+class Singleton:
+    __lock = threading.Lock()
+    __instance = None
+
+    @classmethod
+    def instance(cls):
+        if not cls.__instance:
+            with cls.__lock:
+                if not cls.__instance:
+                    cls.__instance = cls()
+
+
+A = Singleton.instance()
+B = Singleton.instance()
+print(A)
+print(B)
+
