f12e350a3dc13d0e80487b9abd8f568f0106adf1
mejomba <moojaminzadeh@gmail.com>
2022-12-17 16:02:30 +0330

    a

--- Mojtaba_Aminzadeh_hw7_maktab89/4.sh
+++ Mojtaba_Aminzadeh_hw7_maktab89/4.sh
@@ -7,7 +7,6 @@ echo -n "file  name for copy: "
 read directory
 
 for item in "$source_directory"/*; do
-	# file_name=$(echo "$item" | grep a)
 	if [[ -f $item ]] && [[ $(file $item | grep -i "ascii text") ]]; then
 		cat $item >> $directory
 	fi
