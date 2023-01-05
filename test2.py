db006cd450ed82eb381f015fabe0cf236c623dac
mejomba <moojaminzadeh@gmail.com>
2023-01-05 14:09:02 +0330

    cw10

--- test2.py
+++ test2.py
@@ -1,26 +1,17 @@
-class a:
-    def func(self):
-        print("Hello, you are now in class A")
+import unittest
 
 
-class b(a):
-    def func(self):
-        super().func()
-        print("Hello, you are now in class B")
+class CustomAssertions:
+    def my_custom_assertion(self):
+        # check some condition or ...
+        raise AssertionError('this is my custom assertion')
 
 
-class c(a):
-    def func(self):
-        super().func()
-        print("Hello, you are now in class C")
+class TestCase(unittest.TestCase, CustomAssertions):
+
+    def test_my_assertion(self):
+        self.my_custom_assertion()
 
 
-class d(b, c):
-    def func(self):
-        super().func()
-
-
-ref = d()
-ref.func()
-# ============================
-# ============================
+if __name__ == '__main__':
+    unittest.main()
--- test3.py
+++ test3.py
@@ -1,49 +1,17 @@
-class Add:
-
-    def __init__(self, number=0):
-        self.number = number
-
-    def __call__(self, *args, **kwargs):
-        self.number += args[0]
-        return self
-
-    def __repr__(self):
-        return str(self.number)
+import unittest
 
 
-# x = Add()
-# print(x(10)(20)(10)(5)(-2))
-#
-# print(Add(10)(20)(5)(3))
-
-#
-# class Add:
-#
-#     def __init__(self, number=0):
-#         self.number = number
-#
-#     def __call__(self, num):
-#         self.number += num
-#         return self
-#
-#     def __repr__(self):
-#         return str(self.number)
-#
-#
-# x = Add()
-# print(x(10)(20)(10)(5)(-2))
-#
-# print(Add(10)(20)(5)(3))
-
-class add(int):
-    def __call__(self, value):
-        return add(self + value)
+class CustomAssertions:
+    def my_custom_assertion(self):
+        # check some condition or ...
+        raise AssertionError('this is my custom assertion')
 
 
-print(add(5)(10)(20)(4))
+class TestCase(unittest.TestCase, CustomAssertions):
 
-from test import *
-import test
-from test import _a
-print(_a
-)
+    def test_my_assertion(self):
+        self.my_custom_assertion()
+
+
+if __name__ == '__main__':
+    unittest.main()
