908481f6362a70028bdfcb2ce5f81eb7145da16b
mejomba <moojaminzadeh@gmail.com>
2023-01-09 00:16:14 +0330

    test

--- test.py
+++ test.py
@@ -1,19 +1,8 @@
-import requests
+import threading
 
-requests.get('http://google.com')
-def ping(host):
-    import os
-    res = False
-
-    ping_param = "-n 1" if os.name.lower() == "windows" else "-c 1"
-
-
-    resultado = os.system("ping " + ping_param + " " + host)
-    print(resultado)
-
-    if resultado == 0:
-        res = True
-    return res
-
-res = ping('8.8.8.8')
-print(res)
+balance_lock = threading.Lock()
+balance_lock.acquire()
+try:
+    pass
+finally:
+    balance_lock.release()
