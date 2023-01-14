c404c7a62c42348ef772606ef46df0bedcd215a0
mejomba <moojaminzadeh@gmail.com>
2023-01-14 21:08:18 +0330

    cw12

--- maktab89_CW12/1.py
+++ maktab89_CW12/1.py
@@ -5,11 +5,12 @@ class FilteredEditor:
     def __init__(self, path, mode='r'):
         self.path = path
         self.mode = mode
+        self.words = {}
 
     def __enter__(self):
         try:
             self.file = open(self.path, mode=self.mode)
-            return self.file
+            return self
         except FileNotFoundError as e:
             print(e.strerror)
             return None
@@ -20,19 +21,22 @@ class FilteredEditor:
         # print(exc_tb)
         if file:
             self.file.close()
-        return True
+        # return True
+
+    def filter_word(self, user_input):
+        if self.file:
+            for item in self.file:
+                new_item = item.split(',')
+                self.words[new_item[0]] = new_item[1].strip('\n')
+
+            for item in self.words:
+                if item in user_input:
+                    user_input = re.sub(item, self.words.get(item), user_input)
+            return user_input
 
 
 path = 'bad_word.txt'
 user_input = 'salam man biadab hastam to khobi mostafa'
-words = {}
 with FilteredEditor(path, 'r') as file:
-    if file:
-        for item in file:
-            new_item = item.split(',')
-            words[new_item[0]] = new_item[1].strip('\n')
-
-        for item in words:
-            if item in user_input:
-                user_input = re.sub(item, words.get(item), user_input)
-        print(user_input)
+    result = file.filter_word(user_input)
+    print(result)
--- test.py
+++ test.py
@@ -1,19 +1,14 @@
-lst = [1,3,4,5]
 
+x = {}
+x[3] = x[3] = (x, 3)
 
-def iterator(iterable):
-    if len(iterable) > 0:
-        print(iterable[0])
-        # yield iterable[0]
-        return iterator(iterable[1:])
-    # yield iterable[0]
-    # print(iterable[0])
+print(x)
+print(x[3][0])
+print(x[3][0][3][0])
+print(x[3][0][3][0][3][0])
+print(x[3][0][3][0][3][0][3][0])
+print(x[3][0][3][0][3][0][3][0][3][0])
+print(x[3][0][3][0][3][0][3][0][3][0][3][0])
+print(x[3][0][3][0][3][0][3][0][3][0][3][0][3][0])
+print(x[3][0][3][0][3][0][3][0][3][0][3][0][3][0][3][0])
 
-# print(list(iterator(lst)))
-iterator(lst)
-
-
-
-
-
-# a = {7: ({...}, 7)}
