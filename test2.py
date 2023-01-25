35a493eb3ffe3dfd3f8f8ab63b95d47925de4c01
mejomba <moojaminzadeh@gmail.com>
2023-01-25 11:28:26 +0330

    hachi

--- test2.py
+++ test2.py
@@ -1,20 +1,7 @@
-import unittest
+def test():
+    return 'salam'
 
 
-def func():
-    5/0
-    try:
-        x = 5/0
-    except ZeroDivisionError:
-        return 'zero'
 
-
-# print(func())
-
-class TestExcepiont(unittest.TestCase):
-    def test_func(self):
-        self.assertRaises(ZeroDivisionError, func)
-
-
-if __name__ == "__main__":
-    unittest.main()
+x = test()
+print(x)
